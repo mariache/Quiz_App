c9b9f3f20b0ec7a2994b072534fb30183c7a6132
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-13 02:04:00 +1200

    - AnswerItem component;

--- src/components/activeQuiz/answersList/AnswersList.js
+++ src/components/activeQuiz/answersList/AnswersList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import classes from "./AnswersList.module.css ";
+import { AnswerItem } from "./answerItem/AnswerItem";
 
 export const AnswersList = (props) => {
   return (
     <ul className={classes.AnswersList}>
-      <li>{props.answers.map((answer, index) => {})}</li>
+      <li>
+        {props.answers.map((answer, index) => {
+          return <AnswerItem answer={answer} />;
+        })}
+      </li>
     </ul>
   );
 };
---
+++ src/components/activeQuiz/answersList/answerItem/AnswerItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./AnswerItem.module.css";
+
+export const AnswerItem = (props) => {
+  return <li className={classes.AnswerItem}>{props.answer.text}</li>;
+};
+
+export default AnswerItem;
