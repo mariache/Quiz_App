fe0ad0a650484cdaafba78ded5d772fba8e08173
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-14 23:17:04 +1200

    - styling for rigth ang wrong answers;

--- src/components/activeQuiz/answersList/answerItem/AnswerItem.js
+++ src/components/activeQuiz/answersList/answerItem/AnswerItem.js
@@ -2,9 +2,13 @@ import React from "react";
 import classes from "./AnswerItem.module.css";
 
 export const AnswerItem = (props) => {
+  const cls = [classes.AnswerItem];
+  if (props.state) {
+    cls.push(classes[props.state]);
+  }
   return (
     <li
-      className={classes.AnswerItem}
+      className={cls.join(" ")}
       onClick={() => {
         props.onAnswerClick(props.answer.id);
       }}
