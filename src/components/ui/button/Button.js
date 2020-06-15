6989e143e8b82e3ab806e2c689565d82d717070e
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-15 17:55:45 +1200

    - button hoc component;

---
+++ src/components/ui/button/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Button.module.css";
+
+export const Button = (props) => {
+  const cls = [classes.Button];
+  return (
+    <button
+      className={cls.join(" ")}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
