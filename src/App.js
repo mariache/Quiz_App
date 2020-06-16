1afd6d7e45ff04b04bc73a7b0580d2bc893ac28f
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-16 23:09:38 +1200

    - routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Layout from "./hoc/layout/Layout";
 import Quiz from "./containers/quiz/Quiz";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Layout>
-      <div>
-        <Quiz />
-      </div>
+      <Switch>
+        <Route path="/auth" component={Quiz} />
+        <Route path="/quiz-creator" component={Quiz} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/" component={Quiz} />
+      </Switch>
+      <Quiz />
     </Layout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>{app}</React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
