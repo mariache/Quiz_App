e4d5520c5fdad944c903696fc6508b3101b3ab7a
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-17 14:35:35 +1200

    -Input Component;

---
+++ src/components/ui/input/Input.js
@@ -0,0 +1,36 @@
+import React from "react";
+import classes from "./Input.module.css";
+
+function isInvalid({ valid, touched, shouldValidate }) {
+  return !valid && shouldValidate && touched;
+}
+
+const Input = (props) => {
+  const inputType = props.type || "text";
+  const cls = [classes.Input];
+  const htmlFor = `${inputType}-${Math.random()}`;
+
+  if (isInvalid(props)) {
+    cls.push(classes.invalid);
+  }
+
+  return (
+    <div className={cls.join(" ")}>
+      <label htmlFor={htmlFor}>
+        {props.label}
+        <input
+          type={inputType}
+          name=""
+          id={htmlFor}
+          value={props.value}
+          onChange={props.onChange}
+        />
+      </label>
+      {isInvalid(props) ? (
+        <span>{props.errorMessage || "Please enter the value"}</span>
+      ) : null}
+    </div>
+  );
+};
+
+export default Input;
