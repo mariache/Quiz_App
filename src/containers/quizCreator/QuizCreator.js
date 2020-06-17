5184690c593f20d2c004c86896097e3b6609b4cb
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-17 22:36:47 +1200

    addQuiz func

--- src/containers/quizCreator/QuizCreator.js
+++ src/containers/quizCreator/QuizCreator.js
@@ -43,6 +43,7 @@ const selectOptions = [
 export class QuizCreator extends Component {
   state = {
     quiz: [],
+    rigthAnswerId: 1,
     isFormValid: false,
     formControls: createFormControls(),
   };
@@ -53,9 +54,56 @@ export class QuizCreator extends Component {
 
   addQuestionHandler = (event) => {
     event.preventDefault();
+
+    const quiz = this.state.quiz.concat();
+    const index = quiz.length + 1;
+
+    const {
+      question,
+      option1,
+      option2,
+      option3,
+      option4,
+    } = this.state.formControls;
+    const questionItem = {
+      question: question.value,
+      id: index,
+      rightAnswerId: this.state.rigthAnswerId,
+      answers: [
+        {
+          text: option1.value,
+          id: option1.id,
+        },
+        {
+          text: option2.value,
+          id: option2.id,
+        },
+        {
+          text: option3.value,
+          id: option3.id,
+        },
+        {
+          text: option4.value,
+          id: option4.id,
+        },
+      ],
+    };
+
+    quiz.push(questionItem);
+
+    this.setState({
+      quiz,
+      rigthAnswerId: 1,
+      isFormValid: false,
+      formControls: createFormControls(),
+    });
   };
 
-  createQuizHandler = () => {};
+  createQuizHandler = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.quiz);
+  };
 
   changeHandler = (value, controlName) => {
     const formControls = { ...this.state.formControls };
