87d08caea19807fca2b536fe129552030e7aceec
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-17 13:33:58 +1200

    - class instead of function;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from "react";
-import Layout from "./hoc/layout/Layout";
+import React, { Component } from "react";
+import { Layout } from "./hoc/layout/Layout";
+import { Route, Switch } from "react-router-dom";
 import Quiz from "./containers/quiz/Quiz";
 import QuizList from "./containers/quizList/QuizList";
-import QuizCreator from "./containers/quizCreator/QuizCreator";
 import Auth from "./containers/auth/Auth";
-import { Route, Switch } from "react-router-dom";
+import QuizCreator from "./containers/quizCreator/QuizCreator";
 
-function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/auth" component={Auth} />
-        <Route path="/quiz-creator" component={QuizCreator} />
-        <Route path="/quiz/:id" component={Quiz} />
-        <Route path="/" component={QuizList} />
-      </Switch>
-      <Quiz />
-    </Layout>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={QuizList} />
+        </Switch>
+      </Layout>
+    );
+  }
 }
 
 export default App;
