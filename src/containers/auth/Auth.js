acb490ab6701ba606f4b3dc59d9bc5d233e0f8b7
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-17 13:56:13 +1200

    -Auth component;

--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
 import classes from "./Auth.module.css";
+import { Button } from "../../components/ui/button/Button";
 
 export class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Auth</div>;
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Autorisation</h1>
+
+          <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Sign in
+            </Button>
+
+            <Button type="primary" onClick={this.registerHandler}>
+              Sign up
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
