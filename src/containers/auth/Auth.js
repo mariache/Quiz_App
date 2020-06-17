75c0b4d594266224153084bc6733ecee0e9009ee
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-17 15:31:17 +1200

    - form validation/ buttons disabled;

--- src/containers/auth/Auth.js
+++ src/containers/auth/Auth.js
@@ -6,6 +6,7 @@ import Input from "../../components/ui/input/Input";
 
 export class Auth extends Component {
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: "",
@@ -74,8 +75,15 @@ export class Auth extends Component {
 
     formControls[controlName] = control;
 
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
     this.setState({
       formControls,
+      isFormValid,
     });
   };
 
@@ -107,11 +115,19 @@ export class Auth extends Component {
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
             {this.renderInputs()}
 
-            <Button type="success" onClick={this.loginHandler}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Sign in
             </Button>
 
-            <Button type="primary" onClick={this.registerHandler}>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               Sign up
             </Button>
           </form>
