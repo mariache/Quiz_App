5b1ce5671b713c5055d008dc1b85d0262c3264fd
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-22 23:10:10 +1200

    - redirect on logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { logout } from "../store/actions/authActions";
 
 export class Logout extends Component {
+  componentDidMount() {
+    this.props.logout();
+  }
+
   render() {
     return <Redirect to={"/"} />;
   }
 }
 
-export default Logout;
+function mapDispatchToProps(dispatch) {
+  return {
+    logout: () => dispatch(logout()),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Logout);
