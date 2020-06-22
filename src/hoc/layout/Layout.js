ed9257e552f77a8fa5ebf7fcc0c7c6bffad6dad4
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-22 23:18:06 +1200

    Layout connected to store;

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import classes from "./Layout.module.css";
-import MenuToggle from "../../components/navigation/menuToggle/MenuToggle";
-import SideMenu from "../../components/navigation/sideMenu/SideMenu";
+import { MenuToggle } from "../../components/navigation/menuToggle/MenuToggle";
+import { SideMenu } from "../../components/navigation/sideMenu/SideMenu";
+import { connect } from "react-redux";
 
 export class Layout extends Component {
   state = { menu: false };
@@ -19,7 +20,11 @@ export class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
-        <SideMenu isOpen={this.state.menu} onClose={this.menuCloseHandler} />
+        <SideMenu
+          isOpen={this.state.menu}
+          onClose={this.menuCloseHandler}
+          isAuthenticated={this.props.isAuthenticated}
+        />
         <MenuToggle
           onToggle={this.toggleMenuHandler}
           isOpen={this.state.menu}
@@ -29,5 +34,10 @@ export class Layout extends Component {
     );
   }
 }
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token,
+  };
+}
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
