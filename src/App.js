74d67b5f9a4a85af089a229f413e21c17b5dac40
Salam Omarbagaev <omarbagaev@gmail.com>
2021-07-12 21:27:39 +1200

    - App turned to FC;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Layout from "./hoc/Layout/Layout";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import Quiz from "./containers/Quiz/Quiz";
@@ -9,36 +9,34 @@ import { connect } from "react-redux";
 import Logout from "./components/Logout/Logout";
 import { autoLogin } from "./store/actions/auth";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.autoLogin();
-  }
+const App = (props) => {
+  useEffect(() => {
+    props.autoLogin();
+  }, [props]);
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/quiz/:id" component={Quiz} />
+      <Route path="/" exact component={QuizList} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/quiz-creator" component={QuizCreator} />
         <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/logout" component={Logout} />
         <Route path="/" exact component={QuizList} />
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/quiz-creator" component={QuizCreator} />
-          <Route path="/quiz/:id" component={Quiz} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={QuizList} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-
-    return <Layout>{routes}</Layout>;
   }
-}
+
+  return <Layout>{routes}</Layout>;
+};
 
 function mapStateToProps(state) {
   return {
