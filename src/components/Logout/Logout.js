371d79597b655d09c0d729c2165fd9b01c6bfcf4
Salam Omarbagaev <omarbagaev@gmail.com>
2021-07-12 21:41:58 +1200

    turned to FC

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { logout } from "../../store/actions/auth";
 
-class Logout extends Component {
-  componentDidMount() {
-    this.props.logout();
-  }
+const Logout = (props) => {
+  useEffect(() => {
+    props.logout();
+  }, [props]);
 
-  render() {
-    return <Redirect to={"/"} />;
-  }
-}
+  return <Redirect to={"/"} />;
+};
 
 function mapDispatchToProps(dispatch) {
   return {
