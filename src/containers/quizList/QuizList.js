a7276116b7bcc660752cef8913fcb92bff3798a0
Salam Omarbagaev <omarbagaev@gmail.com>
2021-07-12 21:45:19 +1200

    - QuizList turnd to FC;

--- src/containers/quizList/QuizList.js
+++ src/containers/quizList/QuizList.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import classes from "./QuizList.module.css";
 import { NavLink } from "react-router-dom";
 import Loader from "../../components/UI/Loader/Loader";
 import { connect } from "react-redux";
 import { fetchQuizes } from "../../store/actions/quiz";
 
-class QuizList extends Component {
-  renderQuizes() {
-    return this.props.quizes.map((quiz) => {
+const QuizList = (props) => {
+  useEffect(() => {
+    props.fetchQuizes();
+  }, [props]);
+
+  const renderQuizes = () => {
+    return props.quizes.map((quiz) => {
       return (
         <li key={quiz.id}>
           <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
-  }
+  };
 
-  componentDidMount() {
-    this.props.fetchQuizes();
-  }
+  return (
+    <div className={classes.QuizList}>
+      <div>
+        <h1>All quizes</h1>
 
-  render() {
-    return (
-      <div className={classes.QuizList}>
-        <div>
-          <h1>All quizes</h1>
-
-          {this.props.loading && this.props.quizes.length !== 0 ? (
-            <Loader />
-          ) : (
-            <ul>{this.renderQuizes()}</ul>
-          )}
-        </div>
+        {props.loading && props.quizes.length !== 0 ? (
+          <Loader />
+        ) : (
+          <ul>{renderQuizes()}</ul>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 function mapStateToProps(state) {
   return {
