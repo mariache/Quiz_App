bb63d28324f31e2bde5a641d8c2233bf262d6a87
Salam Omarbagaev <omarbagaev@gmail.com>
2021-07-12 21:29:57 +1200

    - Logout  turned to FC;

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { logout } from "../store/actions/authActions";
 
-export class Logout extends Component {
-  componentDidMount() {
-    this.props.logout();
-  }
+export const Logout = (props) => {
+  useEffect(() => {
+    props.logout();
+  }, [props]);
 
-  render() {
-    return <Redirect to={"/"} />;
-  }
-}
+  return <Redirect to={"/"} />;
+};
 
 function mapDispatchToProps(dispatch) {
   return {
